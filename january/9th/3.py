17be8a87bf1a51dde2fdb33b43e39709c66eacc8
karasaev_d <karasaev_d@bankrbk.kz>
2023-01-09 22:35:28 +0600

    a

---
+++ january/9th/3.py
@@ -0,0 +1,2 @@
+name = 'Dauren'
+print('My name is ' + name)
---
+++ january/9th/4.py
@@ -0,0 +1,6 @@
+# int c = 3; integer - целое число
+# string c x
+c = 3
+c = 'Atyray'
+# int, float - дробные числа: 0.1, string - строки
+print(float(3))
