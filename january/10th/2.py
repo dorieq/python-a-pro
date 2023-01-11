3643aad8a14ab24f302fd19172cbd06fe51590ca
karasaev_d <karasaev_d@bankrbk.kz>
2023-01-11 13:26:42 +0600

    foo

---
+++ january/10th/2.py
@@ -0,0 +1,5 @@
+a = input()
+print(a)
+
+# a <- console
+# a -> console
---
+++ january/10th/3.py
@@ -0,0 +1,7 @@
+a = 3
+# int a = 3; int - integer - целое число
+# a = "abc"; integer -> string
+a = 'abc'
+# integer - целое число(1, 2, -5, 0), float - дробные числа(0.5, -1.5, 30.4), boolean - логический(True, False), string - строка('asdcf', '123')
+a = str(4) + ' is four'
+print(a)
